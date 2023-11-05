16e4b4bff0b9f07e0bbdd17275f7875a26433abe
Francesco Sapia <123062954+sapiaf@users.noreply.github.com>
2023-11-05 22:34:08 +0100

    TinyMCE implemented

--- src/main/java/dev/sapia/blog/controller/IndexController.java
+++ src/main/java/dev/sapia/blog/controller/IndexController.java
@@ -5,9 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 @Controller
 @RequestMapping("/")
 public class IndexController {

c36af9f2727dc04eb28be6a586c560f6fd152060
Francesco Sapia <123062954+sapiaf@users.noreply.github.com>
2023-11-17 11:31:55 +0100

    Post create, delete, update finished

--- src/main/java/dev/sapia/blog/controller/admin/AdminController.java
+++ src/main/java/dev/sapia/blog/controller/admin/AdminController.java
@@ -1,23 +1,35 @@
 package dev.sapia.blog.controller.admin;
 
+import dev.sapia.blog.model.Category;
 import dev.sapia.blog.model.Post;
+import dev.sapia.blog.model.User;
+import dev.sapia.blog.repository.CategoryRepository;
 import dev.sapia.blog.repository.PostRepository;
+import dev.sapia.blog.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
     @Autowired
     private PostRepository postRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
     public String index(Model model) {
@@ -28,11 +40,56 @@ public class AdminController {
 
     @GetMapping("/post/create")
     public String create(Model model) {
-        return "post/create";
+        List<Category> categories = categoryRepository.findAll();
+        model.addAttribute("categories", categories);
+        return "admin/post/create";
     }
     @PostMapping("/post/create")
-    public String doCreate(@Valid @ModelAttribute("post") Post post) {
+    public String doCreate(@Valid @ModelAttribute("post") Post post, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "admin/post/create";
+        }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String loggedInUserEmail = authentication.getName();
+        Optional<User> loggedInUserOptional = userRepository.findByEmail(loggedInUserEmail);
+        if (loggedInUserOptional.isPresent()) {
+            User loggedInUser = loggedInUserOptional.get();
+            post.setAuthor(loggedInUser);
+            post.setDate(LocalDate.now());
+            postRepository.save(post);
+            return "redirect:/admin";
+        } else {
+            return "redirect:/login";
+        }
+    }
+
+    @GetMapping("/post/update/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Optional<Post> post = postRepository.findById(id);
+        if (post.isPresent()) {
+            model.addAttribute("post", post.get());
+            model.addAttribute("categories", categoryRepository.findAll());
+            return "admin/post/edit";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "post with id " + id + " not found");
+        }
+    }
+
+    @PostMapping("/post/update/{id}")
+    public String doEdit(@PathVariable Integer id, @Valid @ModelAttribute("post") Post post,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "admin/post/edit";
+        }
+        post.setDate(LocalDate.now());
         postRepository.save(post);
-        return "redirect:/";
+        return "redirect:/admin";
+    }
+
+
+    @PostMapping("/post/delete/{id}")
+    public String deleteById(@PathVariable Integer id) {
+        postRepository.deleteById(id);
+        return "redirect:/admin";
     }
 }
